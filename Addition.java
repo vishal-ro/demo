7dbecd72dcf8960b1b53b8303b1a1557f9b1de59
vishal-ro <155897574+vishal-ro@users.noreply.github.com>
2024-05-01 15:52:35 +0530

    Update Addition.java

--- Addition.java
+++ Addition.java
@@ -1,14 +1,8 @@
-import java.util.Scanner;
-
 public class Addition {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter the first number: ");
-        int number1 = scanner.nextInt();
-        System.out.print("Enter the second number: ");
-        int number2 = scanner.nextInt();
+        int number1 = 5;
+        int number2 = 3;
         int sum = number1 + number2;
         System.out.println("The sum of " + number1 + " and " + number2 + " is: " + sum);
-        scanner.close();
     }
 }
